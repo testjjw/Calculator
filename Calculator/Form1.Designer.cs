a9e0a9898ca045fa840e58861a906dda74c18e6e
jwjung1219 <jwjung1219@eotechnics.com>
2026-02-11 14:25:19 +0900

    CALC-3 test

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             tbInsert1 = new TextBox();
             tbInsert2 = new TextBox();
+            textBox1 = new TextBox();
             SuspendLayout();
             // 
             // tbInsert1
@@ -46,11 +47,19 @@
             tbInsert2.Size = new Size(66, 23);
             tbInsert2.TabIndex = 1;
             // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(22, 70);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(66, 23);
+            textBox1.TabIndex = 2;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
             Controls.Add(tbInsert2);
             Controls.Add(tbInsert1);
             Name = "Form1";
@@ -63,5 +72,6 @@
 
         private TextBox tbInsert1;
         private TextBox tbInsert2;
+        private TextBox textBox1;
     }
 }
