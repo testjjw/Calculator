48c33ec6c216261a287c2d546149bf727f6706bd
jwjung1219 <jwjung1219@eotechnics.com>
2026-02-11 15:42:49 +0900

    CALC-3 입력 UI 추가 수정

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             tbInsert1 = new TextBox();
             tbInsert2 = new TextBox();
-            textBox1 = new TextBox();
+            tbInsert3 = new TextBox();
             SuspendLayout();
             // 
             // tbInsert1
@@ -49,19 +49,20 @@
             tbInsert2.TabIndex = 1;
             tbInsert2.Text = "5";
             // 
-            // textBox1
+            // tbInsert3
             // 
-            textBox1.Location = new Point(12, 70);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(66, 23);
-            textBox1.TabIndex = 2;
+            tbInsert3.Location = new Point(12, 70);
+            tbInsert3.Name = "tbInsert3";
+            tbInsert3.Size = new Size(66, 23);
+            tbInsert3.TabIndex = 2;
+            tbInsert3.Text = "5";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(textBox1);
+            Controls.Add(tbInsert3);
             Controls.Add(tbInsert2);
             Controls.Add(tbInsert1);
             Name = "Form1";
@@ -74,6 +75,6 @@
 
         private TextBox tbInsert1;
         private TextBox tbInsert2;
-        private TextBox textBox1;
+        private TextBox tbInsert3;
     }
 }
